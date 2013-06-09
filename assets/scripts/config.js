b2b28013a951d0df6c3f57763f3184af8484fecc
sirprize <hoegl@backdoor.ch>
2013-06-10 00:25:55 +0200

    Update vendors

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,6 +3,8 @@ var dojoConfig = {
     //cacheBust: 1,
     parseOnLoad: true,
     packages: [
+        { name: "mijit", location: "../../sirprize/mijit" },
+        { name: "dojo-form-controls", location: "../../sirprize/dojo-form-controls" },
         { name: "dobolo", location: "../../sirprize/dobolo" },
         { name: "frontend", location: "../../../scripts" }
     ]
